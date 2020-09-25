940116a4b3c447d838fa64ee3ebf6b2092ecc9d2
Vladislav <zippo585@gmail.com>
2020-09-25 16:31:53 +0500

    Nav component finished

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,8 +3,20 @@ import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 100) {
+        setShow(true)
+      }else setShow(false);
+    });
+      return () => {
+        window.removeEventListener("scroll");
+      }
+    }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img
         className="nav__logo"
         src="http://static.wixstatic.com/media/cbafe1_63c8e817ce56404193de0db66bc66418~mv2_d_1600_1600_s_2.png"
